60a3d0c60e592b451f5633f55fb392cffb3b82b7
drakkomaximo <ing.villanuevatt@gmail.com>
2020-02-10 00:44:11 -0500

    Use de la database firebase en el proyecto

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,3 +1,4 @@
+import { Equipo } from './../interfaces/equipo.interface';
 import { infoPages } from './../interfaces/indo-pages.interface';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -8,19 +9,34 @@ import { HttpClient } from '@angular/common/http';
 export class InfoPaginaService {
 
   public info: infoPages
+  public infoEquipo: Equipo
   public cargado: boolean = false
+  public cargadoEquipo: boolean = false
+  public urlEquipo: string = "https://angular-html-andres.firebaseio.com/equipo.json"
 
-  constructor(private _http:HttpClient) { 
+  constructor(private _http: HttpClient) {
+    this.cargarInfo()
+    this.cargarEquipo()
+  }
 
-    /* console.log('Servicio de Página') */
-
+  private cargarInfo() {
     //Leer el archivo Json
     this._http.get('assets/data/data-pages.json').subscribe(
-      (resp: infoPages)=> {
+      (resp: infoPages) => {
         this.cargado = true
         this.info = resp
+        console.log(this.info)
       }
     )
+  }
 
+  private cargarEquipo(){
+    this._http.get(this.urlEquipo).subscribe(
+      (resp: Equipo) => {
+        this.cargadoEquipo = true
+        this.infoEquipo = resp
+        console.log(this.infoEquipo)
+      }
+    )
   }
 }
